ea0778222b124243839d96842cd6f67819b6b7a8
sliim35 <slim.r35@gmail.com>
2018-12-18 21:21:51 +0300

    Connect API

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-// Global app controller
+import axios from 'axios';
+
+async function getResult(query) {
+  const key = '75f5a247a695f874d3c6da3d9b65b1c6';
+  try {
+    const res = await axios(
+      `https://www.food2fork.com/api/search?key=${key}&q=${query}`,
+    );
+    const { recipes } = res.data;
+    console.log(recipes);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getResult('tomato pasta');
