f5e6d1c5092d4677f34de7adcf549d8947809a95
sliim35 <slim.r35@gmail.com>
2018-12-18 21:44:39 +0300

    Search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,28 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
+/**
+ * State:
+ * - Search
+ * - Current recipe
+ * - Shopping list
+ * - Liked
+ */
+const state = {};
 
-search.getResults();
-console.log(search);
+const searchCtrl = async () => {
+  const query = 'pizza';
+
+  if (query) {
+    state.search = new Search(query);
+    await state.search.getResults();
+
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', submitHandler);
+
+function submitHandler(e) {
+  e.preventDefault();
+  searchCtrl();
+}
